256019907b131ebbe4f842c877c478d3def92b83
Ayyan Ali Khan <ayyanalikhan510@gmail.com>
2024-10-19 00:05:57 +0500

    update slug page

--- app/countries/[country]/page.tsx
+++ app/countries/[country]/page.tsx
@@ -1,6 +1,5 @@
 import CountryData from '@/app/(data)/CountryData';
 import CountryCard from '@/components/custom/CountryCard';
-import Link from 'next/link'
 
 interface Params {
   params:{
@@ -26,7 +25,6 @@ export default  function CountryPage({params}:Params) {
 
   const [{ country , population , capital , region }] = selectedCountryData;
 
-  console.log(selectedCountryData)
 
   return (
     <CountryCard country={country} population={population} region={region} capital={capital} />
